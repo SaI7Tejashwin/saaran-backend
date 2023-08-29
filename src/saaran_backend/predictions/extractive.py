dfa865149b0f919dbdc4a080e7cd283f61ec6d50
SaI7Tejashwin <esaitejashwin@gmail.com>
2023-08-29 18:52:25 +0530

    Added all extractive summarization models

--- src/saaran_backend/predictions/extractive.py
+++ src/saaran_backend/predictions/extractive.py
@@ -2,13 +2,27 @@ from fastapi import APIRouter, BackgroundTasks
 from pydantic import BaseModel
 
 #util ML functions
-from .extractive_utils.sumy import sumy_lex_summarize
+from .extractive_utils.sumy import (
+    sumy_kl_summarize,
+    sumy_lex_summarize,
+    sumy_luhn_summarize,
+    sumy_lsa_summarize
+    )
 
 router = APIRouter(
     prefix="/predict/extract",
     tags = ["extract"],
     responses = {404: {'description': "Not found"}}
 )
+
+'''
+Need to define a input body such as 
+{
+    file: ...., //probably base-64 encoded
+    sentences: ...,
+    extention: ... #To handle the file-types
+}
+'''
 
 class InputText(BaseModel):
     text: str
@@ -17,7 +31,22 @@ class InputText(BaseModel):
 def get_extactive():
     return {"model_type": "extractive"}
 
-@router.post('/sumy')
-async def predict_sumy(input_text: InputText, background_tasks: BackgroundTasks):
+@router.post('/sumy-lex')
+async def predict_sumy_lex(input_text: InputText, background_tasks: BackgroundTasks):
     summary = sumy_lex_summarize(input_text)
+    return {"summary": summary, "message": "text summarized"}
+
+@router.post('/sumy-luhn')
+async def predict_sumy(input_text: InputText, background_tasks: BackgroundTasks):
+    summary = sumy_luhn_summarize(input_text)
+    return {"summary": summary, "message": "text summarized"}
+
+@router.post('/sumy-kl')
+async def predict_sumy(input_text: InputText, background_tasks: BackgroundTasks):
+    summary = sumy_kl_summarize(input_text)
+    return {"summary": summary, "message": "text summarized"}
+
+@router.post('/sumy-lsa')
+async def predict_sumy(input_text: InputText, background_tasks: BackgroundTasks):
+    summary = sumy_lsa_summarize(input_text)
     return {"summary": summary, "message": "text summarized"}
--- src/saaran_backend/predictions/extractive_utils/sumy.py
+++ src/saaran_backend/predictions/extractive_utils/sumy.py
@@ -4,18 +4,52 @@ import sumy
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.kl import KLSummarizer
 
-# nltk.download('punkt')
+# nltk.download('punkt') --> WHY IS THIS STALLING MY SERVER WTF --> ONE TIME USE ONLY
+# WARNING: This is needed so open a py shell in the venv and type this line
 
-lex_rank_summarizer = LexRankSummarizer()
-
+#Load the models
 def sumy_lex_summarize(input_text: str) -> str:
+    lex_rank_summarizer = LexRankSummarizer()
     summ_text = ""
-    my_parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
-    lexrank_summary = lex_rank_summarizer(my_parser.document, sentences_count=3)
-
+    parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
+    lexrank_summary = lex_rank_summarizer(parser.document, sentences_count=3)
     for sentence in lexrank_summary:
         summ_text += str(sentence)
 
     return summ_text
     
+def sumy_luhn_summarize(input_text: str) -> str:
+    luhn_summarizer = LuhnSummarizer()
+    summ_text = ""
+    parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
+    luhn_summary = luhn_summarizer(parser.document, sentences_count=3)
+
+    for sentence in luhn_summary:
+        summ_text += str(sentence)
+    return summ_text
+
+def sumy_kl_summarize(input_text: str) -> str:
+    summ_text = ""
+    kl_summarizer = KLSummarizer()
+    parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
+    kl_summary = kl_summarizer(parser.document, sentences_count=3)
+
+    for sentence in kl_summary:
+        summ_text += str(sentence)
+
+    return summ_text
+
+def sumy_lsa_summarize(input_text: str) -> str:
+    lsa_summarizer = LsaSummarizer()
+    summ_text = ""
+    parser = PlaintextParser.from_string(input_text, Tokenizer('english'))
+    lsa_summary = lsa_summarizer(parser.document, sentences_count=3)
+
+    for sentence in lsa_summary:
+        summ_text += str(sentence)
+
+    return summ_text
